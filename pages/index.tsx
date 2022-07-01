1d7239633867b3fafbdb0d40238c59fcde316261
Carl Chen <ayang879@gmail.com>
2022-07-01 12:27:52 +0800

    fix: use isDesktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,24 @@
-import { useIsSmallScreen } from 'hooks/isSmallScreen'
 import React, { useEffect, useState, Suspense, lazy, useMemo } from 'react'
 import { Text } from 'rebass'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { parse } from 'next-useragent'
+import { parse, UserAgent } from 'next-useragent'
 
 const Desktop = lazy(() => import('components/desktop'))
 const Mobile = lazy(() => import('components/mobile'))
 
-export default function Home({ isMobile }: { isMobile: boolean }) {
+export default function Home({ isMobile, isTablet, isDesktop }: UserAgent) {
   useEffect(() => {
     import('utils/skynet').then((skynet) => {
       skynet.default.start()
     })
   }, [])
 
-  return <Suspense fallback={() => <Text>loading</Text>}>{isMobile ? <Mobile /> : <Desktop />}</Suspense>
+  return <Suspense fallback={() => <Text>loading</Text>}>{isDesktop ? <Desktop /> : <Mobile />}</Suspense>
 }
 
 export async function getServerSideProps({ req, res }: { req: NextApiRequest; res: NextApiResponse }) {
   const ua = parse(req.headers['user-agent'] || '')
   return {
-    props: { isMobile: ua.isMobile },
+    props: ua,
   }
 }
