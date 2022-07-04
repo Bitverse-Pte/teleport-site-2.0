3d5c121f896a4655b248a28a00c8567c5a13e84b
Carl Chen <ayang879@gmail.com>
2022-07-04 18:20:11 +0800

    fix: foot hover color

--- components/Footer/desktop.tsx
+++ components/Footer/desktop.tsx
@@ -1,16 +1,50 @@
 import React, { memo } from 'react'
 import { FooterStyled } from './styled'
-import Link from 'next/link'
+import Link, { LinkProps } from 'next/link'
 import Image from 'components/Image'
-import { Flex } from 'rebass/styled-components'
+import { Flex, Text } from 'rebass/styled-components'
+import styled from 'styled-components'
 
 import teleportHorizontal from 'public/images/teleport-horizontal.svg'
+
+const StyledLink = (props: { text: string } & LinkProps) => {
+  return (
+    <Link {...props} passHref={true}>
+      <Text
+        sx={{
+          fontFamily: 'Poppins',
+          fontStyle: 'normal',
+          fontWeight: '400',
+          fontSize: '18px',
+          lineHeight: '27px',
+          color: '#000000',
+          marginBottom: '16px',
+          cursor: 'pointer',
+          ':hover': {
+            color: '#59C3AA',
+          },
+        }}
+      >
+        {props.text}
+      </Text>
+    </Link>
+  )
+}
 
 const Footer = () => {
   return (
     <FooterStyled>
       <div className="wrapFooter centerView">
-        <Flex flexDirection={'row'} justifyContent={'space-between'}>
+        <Flex
+          flexDirection={'row'}
+          justifyContent={'space-between'}
+          sx={{
+            transition: 'all 0.2s ease-in-out',
+            'a:hover': {
+              color: '#59c3aa',
+            },
+          }}
+        >
           <Link href="/">
             <a>
               <Image className="imgLogoHorizontal" src={teleportHorizontal} alt={'footer-horizontal-line'} />
@@ -19,21 +53,13 @@ const Footer = () => {
           <Flex flexDirection={'row'} justifyContent={'flex-end'} className={'wrapLinks'}>
             <Flex flexDirection={'column'} className={'wrapColumn'}>
               <div className={'textTitle'}>Apply</div>
-              <Link href={'https://ambassador.teleport.network/'} key={'https://ambassador.teleport.network/'}>
-                <a target={'_blank'}>Ambassador</a>
-              </Link>
+              <StyledLink href={'https://ambassador.teleport.network/'} key={'https://ambassador.teleport.network/'} text="Ambassador" />
             </Flex>
             <Flex flexDirection={'column'} className={'wrapColumn'}>
               <div className={'textTitle'}>Developer</div>
-              <Link href={'https://docs.teleport.network/'} key={'https://docs.teleport.network/'}>
-                <a target={'_blank'}>Document</a>
-              </Link>
-              <Link href={'https://github.com/teleport-network'} key={'https://github.com/teleport-network'}>
-                <a target={'_blank'}>Github</a>
-              </Link>
-              <Link href={'https://discord.gg/5YQtRDF4Rh'} key={'https://discord.gg/5YQtRDF4Rh'}>
-                <a target={'_blank'}>Community (Discord)</a>
-              </Link>
+              <StyledLink href={'https://ambassador.teleport.network/'} key={'https://ambassador.teleport.network/'} text="Document" />
+              <StyledLink href={'https://ambassador.teleport.network/'} key={'https://ambassador.teleport.network/'} text="Github" />
+              <StyledLink href={'https://ambassador.teleport.network/'} key={'https://ambassador.teleport.network/'} text="Community (Discord)" />
             </Flex>
           </Flex>
         </Flex>
