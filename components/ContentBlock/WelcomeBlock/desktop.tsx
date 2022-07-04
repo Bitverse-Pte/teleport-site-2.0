b69d28ae8927eb366c3738514f40dd5bee427c75
Carl Chen <ayang879@gmail.com>
2022-07-04 18:19:57 +0800

    feat: better welcome block

--- components/ContentBlock/WelcomeBlock/desktop.tsx
+++ components/ContentBlock/WelcomeBlock/desktop.tsx
@@ -8,48 +8,60 @@ import welcomeGif from 'public/gif/welcome.gif'
 import welcomeTeleport from 'public/welcome-teleport.svg'
 import welcomeSubTitle from 'public/welcome-sub-title.svg'
 import { css } from 'styled-components'
+import { useEffect, useRef, useState } from 'react'
 
 export default function WelcomeBlock() {
+  const containerRef = useRef<HTMLDivElement>()
+  const [containerWidth, setContainerWidth] = useState<number>(0)
+  useEffect(() => {
+    const onResize = function () {
+      containerRef.current && setContainerWidth(containerRef.current!.offsetWidth)
+    }
+    containerRef.current && setContainerWidth(containerRef.current!.offsetWidth)
+    window.addEventListener('resize', onResize)
+    return () => {
+      window.removeEventListener('resize', onResize)
+    }
+  }, [])
   return (
     <Flex
-      flexDirection={'column'}
+      ref={containerRef}
       sx={{
-        position: 'relative',
         width: '100%',
-        height: '288px',
-        justifyContent: 'space-between',
-        transitionDelay: '0s',
+        height: '300px',
+        position: 'relative',
+        flexDirection: 'column',
+        transformOrigin: 'center',
         transition: 'all 0.2s ease-in-out',
-        '.vanish-to-left': {
-          transition: 'all 0.2s ease-in-out',
-        },
-        ':after': {
-          borderBottom: 'solid 2px white',
-        },
+        justifyContent: 'space-between',
         ':hover': {
-          height: '560px',
-          '@media screen and (max-width: 1300px)': {
-            height: '480px',
+          height: '480px',
+          '.welcome-teleport': {
+            transform: `translateX(calc(${(containerWidth ? containerWidth : 0) / 2}px - 50%)) translateY(-20%)`,
           },
-          '.vanish-to-left': {
+          '.welcome-subtitle': {
             transform: 'translateX(-100px)',
-            opacity: '0',
-            // width: 0,
+            opacity: 0,
           },
           '.vanish-to-right': {
-            transform: 'scale(1.6) translateX(100px)',
+            transform: 'translateX(100%)',
             opacity: '0',
+          },
+          '.welcome-gif': {
+            opacity: 1,
+            height: '100%',
+            width: '100%',
+            transform: 'translateY(-10%)',
           },
           '.with-after-border:after': {
             transform: 'scale(0)',
           },
-          '.vertical-grow': {
-            // transform: 'scaleY(1)',
-            height: 'fit-content',
-            opacity: 1,
+          '.vanish-to-left': {
+            transform: 'translateX(-100px)',
+            opacity: '0',
           },
           '.translate-to-left': {
-            transform: 'translateX(-61px)',
+            transform: 'translateX(-45px)',
           },
           '.flatten-div': {
             '.shrink-button': {
@@ -67,51 +79,79 @@ export default function WelcomeBlock() {
               minWidth: '380px!important',
             },
           },
-          /* '.move-down': {
-            transform: 'translateY(230px)',
-          }, */
         },
       }}
     >
-      <Flex flexDirection="row" alignItems={'space-between'}>
-        <Flex flex={5} flexDirection="column" justifyContent={'flex-start'}>
-          <Image src={welcomeTeleport} alt="welcome-main-title" layout="responsive" objectFit="contain"></Image>
-          <br />
-          <Box className="vanish-to-left">
+      <Flex minHeight="160px">
+        <Flex flexDirection={'column'} width="84%" justifyContent={'space-between'}>
+          <Box
+            className="welcome-teleport"
+            sx={{
+              left: 0,
+              transformOrigin: 'center',
+              transition: 'all 0.2s ease-in-out',
+              height: '100px',
+              width: '100%',
+              /*   maxWidth: '720px',
+              minWidth: '720px',
+              '@media screen and (max-width: 1300px)': {
+                minWidth: '640px',
+                maxWidth: '640px',
+              }, */
+            }}
+          >
+            <Image src={welcomeTeleport} alt="welcome-main-title" layout="responsive" objectFit="contain"></Image>
+          </Box>
+          <Box
+            className="welcome-subtitle"
+            sx={{
+              minWidth: `${containerWidth * 0.85}px`,
+              width: '100%',
+              height: '60px',
+              transition: 'all 0.2s ease-in-out',
+              left: 0,
+              top: '100px',
+            }}
+          >
             <Image src={welcomeSubTitle} alt="welcome-sub-title" layout="responsive" objectFit="contain"></Image>
           </Box>
         </Flex>
-        <Flex flex={1} height="100%" sx={{ position: 'relative' }}>
+        <Box
+          sx={{
+            position: 'relative',
+            width: '20%',
+            minWidth: '200px',
+            height: '200px',
+            transition: 'all 0.2s ease-in-out',
+          }}
+          className="vanish-to-right"
+        >
           <Box
-            className={'vanish-to-right'}
             sx={{
               position: 'absolute',
-              top: '-50%',
-              right: 0,
+              top: '-15%',
+              right: '-30%',
               width: '100%',
+              minWidth: '200px',
               height: '100%',
-              transform: 'scale(1.6)',
-              transformOrigin: '50% 0',
               transition: 'all 0.2s ease-in-out',
             }}
           >
-            <Image src={circledT} alt="welcome-gif" layout="fill" objectFit="contain" /* objectPosition={'right -50px'} */ quality={100}></Image>
+            <Image src={circledT} alt="circled-T" layout="fill" objectFit="contain" /* objectPosition={'right -50px'} */ quality={100}></Image>
           </Box>
-        </Flex>
+        </Box>
       </Flex>
+
       <Box
-        className="vertical-grow"
+        className="welcome-gif"
         sx={{
           height: 0,
           opacity: 0,
-          position: 'absolute',
-          top: '140px',
-          '@media screen and (max-width: 1300px)': {
-            top: '120px',
-          },
-          left: '0px',
-          transition: 'all 0.2s ease-in-out',
-          transformOrigin: '0% 50%',
+          width: '0%',
+          // transform: 'scaleY(0)',
+          top: '100px',
+          transition: 'all 0.2s ease-in-out, width 1ms',
+          transformOrigin: 'center',
         }}
       >
         <Image src={welcomeGif} alt="welcome-gif"></Image>
@@ -119,13 +159,19 @@ export default function WelcomeBlock() {
       <Flex
         sx={{
           justifyContent: 'space-between',
-          marginTop: 10,
+          marginTop: 30,
           transition: 'all 0.2s ease-in-out',
         }}
       >
         <Flex
           className="with-after-border"
-          flexDirection={'column'}
+          flexDirection={'row'}
+          justifyContent="flex-start"
+          alignItems={'baseline'}
+          sx={{
+            transition: 'all 0.2s ease-in-out',
+            transformOrigin: '0% 50%',
+          }}
           css={css`
             &:after {
               margin-top: 10px;
@@ -138,31 +184,29 @@ export default function WelcomeBlock() {
             }
           `}
         >
-          <Flex alignItems={'flex-end'} minWidth="480px">
-            <Box
-              className="vanish-to-left"
-              sx={{
-                transformOrigin: '50% 0',
-                transition: 'all 0.2s ease-in-out',
-              }}
-            >
-              <Image src={leftBottomArrow} alt="welcome-gif"></Image>
-            </Box>
-            <Box
-              className="translate-to-left"
-              sx={{
-                transition: 'all 0.2s ease-in-out',
-                transformOrigin: '0% 50%',
-              }}
-            >
-              <Image src={web3Svg} alt="welcome-gif"></Image>
-            </Box>
-          </Flex>
+          <Box
+            className="vanish-to-left"
+            sx={{
+              transformOrigin: '50% 0',
+              transition: 'all 0.2s ease-in-out',
+            }}
+          >
+            <Image src={leftBottomArrow} alt="welcome-gif"></Image>
+          </Box>
+          <Box
+            className="translate-to-left"
+            sx={{
+              transition: 'all 0.2s ease-in-out',
+              transformOrigin: '0% 50%',
+            }}
+          >
+            <Image src={web3Svg} alt="welcome-gif"></Image>
+          </Box>
         </Flex>
         <Flex
           flex={1}
           alignItems="center"
-          justifyContent={'flex-end'}
+          justifyContent={'space-around'}
           className="flatten-div"
           flexWrap="wrap"
           sx={{
@@ -212,7 +256,6 @@ export default function WelcomeBlock() {
                 maxWidth: '155px',
                 fontSize: '20px',
               },
-              marginLeft: 10,
               transition: 'all 0.2s ease-in-out',
               fontFamily: 'IBM Plex Sans',
               fontStyle: 'normal',
