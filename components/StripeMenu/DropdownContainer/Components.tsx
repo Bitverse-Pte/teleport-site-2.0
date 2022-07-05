cc5de3a2c58a8cbf9ebc2a206c8d7a68da7669ab
Carl Chen <ayang879@gmail.com>
2022-07-05 15:44:54 +0800

    feat: remove backdrop in desktop dropdownmenu

--- components/StripeMenu/DropdownContainer/Components.tsx
+++ components/StripeMenu/DropdownContainer/Components.tsx
@@ -45,9 +45,9 @@ export const Caret = styled.div`
 
 export const DropdownBackground = styled.div`
   transform-origin: 0 0;
-  // background: rgba(30, 30, 30, 0.9);
-  background: rgba(255, 255, 255, 0.1);
-  backdrop-filter: blur(79px);
+  background: rgba(30, 30, 30, 1);
+  // background: rgba(255, 255, 255, 0.1);
+  // backdrop-filter: blur(79px);
   mix-blend-mode: normal;
   color: #ffffff;
   border-radius: 12px;
